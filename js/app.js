68dd559bbd84a33690970471e45ed9d9e3545f36
Jeremy <jeremyash13@gmail.com>
2018-09-24 20:55:49 -0600

    App is nearly complete

--- js/app.js
+++ js/app.js
@@ -10,23 +10,86 @@ const phrases = [
   'EVERY DOG HAS ITS DAY',
   'GET A WHIFF OF THIS',
 ];
+
 const startGameButton = document.querySelector('.btn__reset');
 const buttons = document.querySelectorAll('#qwerty button');
-let lives = 5;
+const overlay = document.querySelector('#overlay');
+const qwertySection = document.querySelector('#qwerty');
+const phraseSection = document.querySelector('#phrase');
+const scoreboardSection = document.querySelector('#scoreboard');
+let gamePhrase = undefined;
+let missed = 0;
 
-const displayKeyboard = () => {
-  for (let i = 0; i < buttons.length; i+=1) {
-    const button = buttons[i];
 
+const getRandomPhraseAsArray = (arr) => {
+  const index = Math.floor((Math.random() * 10) + 1);
+  const phrase = Array.from(arr[index]);
+  return phrase;
+};
+
+const addPhraseToDisplay = (arr) => {
+  const ul = phraseSection.lastElementChild;
+  for (let i = 0; i < arr.length; i += 1) {
+    const li = document.createElement('li');
+    li.textContent = arr[i];
+    ul.appendChild(li);
+    if (gamePhrase[i] !== ' ') {
+      li.className += 'letter';
+    } else {
+      li.className += 'space';
+    }
   }
 };
 
-const pickRandom = () => {
-
+const checkLetter = (letter) => {
+  const liCollection = phraseSection.firstElementChild.children;
+  if (gamePhrase.indexOf(letter.toUpperCase()) != -1){
+    for (let i = 0; i < gamePhrase.length; i += 1) {
+      if (gamePhrase[i] === letter.toUpperCase()) {
+        liCollection[i].className += ' show';
+      }
+    }
+  } else {
+    missed += 1;
+    const hearts = scoreboardSection.firstElementChild;
+    hearts.removeChild(hearts.lastElementChild);
+  }
 };
 
-let gamePhrase = phrases[pickRandom()];
+const checkWin = () => {
+  const letterCollection = phraseSection.querySelectorAll('.letter');
+  const showCollection = phraseSection.querySelectorAll('.show');
+  if (letterCollection.length === showCollection.length) {
+    return true;
+  } else if (missed === 5) {
+    return false;
+  }
+};
 
 startGameButton.addEventListener('click', () => {
-  displayKeyboard();
+  if (startGameButton.textContent === 'Play Again') {
+    window.location.reload();
+  } else {
+    overlay.style.display = 'none';
+    gamePhrase = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(gamePhrase);
+  }
+});
+
+qwertySection.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const letter = e.target.textContent;
+    checkLetter(letter);
+    e.target.className += 'chosen';
+    e.target.setAttribute('disabled', 'true');
+    if (checkWin()) {
+      overlay.style.display = '';
+      overlay.className += ' win';
+      startGameButton.textContent = 'Play Again';
+    } else if (checkWin() === false) {
+      overlay.style.display = '';
+      overlay.className += ' lose';
+      startGameButton.textContent = 'Play Again';
+    }
+  }
 });
